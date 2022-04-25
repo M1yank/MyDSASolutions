3fc8017abc86bdbfb60f382b42916280b78ecee3
Mayank <mayank.bansal16901@gmail.com>
2022-04-25 18:02:29 +0530

    Bit Magic

---
+++ 2. Bit Magic/4. power of two.cpp
@@ -0,0 +1,51 @@
+// { Driver Code Starts
+//Initial Template for C++
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+//User function Template for C++
+
+class Solution{
+    public:
+    // Function to check if given number n is a power of two.
+    bool isPowerofTwo(long long n){
+        
+        return(n!=0 && (n&(n-1))==0) ;   
+
+/*//one liner code based on:
+////if n is power of 2, (n&n-1) gives us 0.
+////Ex: 4 : 0100		6 :	0110
+		3 :	0010		5 : 0101
+		& : 0000		& : 0100
+*/ 
+    }
+};
+
+// { Driver Code Starts.
+
+// Driver code
+int main()
+{
+
+    int t;
+    cin>>t;//testcases
+
+    for(int i=0;i<t;i++)
+    {
+        long long n; //input a number n
+        cin>>n;
+
+        Solution ob;
+         if(ob.isPowerofTwo(n))//Now, if log2 produces an integer not decimal then we are sure raising 2 to this value
+             cout<<"YES"<<endl;
+         else
+            cout<<"NO"<<endl;
+
+    }
+
+    return 0;
+}
+  // } Driver Code Ends
