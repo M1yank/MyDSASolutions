a60bc9d11dc76ed4bcd9e0a76925732faf203666
Mayank <mayank.bansal16901@gmail.com>
2022-04-25 17:40:59 +0530

    Bit Magic

--- 2. Bit Magic/2. Count total no. of set bits.cpp
+++ 2. Bit Magic/2. Count total no. of set bits.cpp
@@ -1,8 +1,29 @@
-int countBits(int n){
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+		int n=8;
         int res=0;
-        while(i>0){
-            i=(i&(i-1));
+        while(n>0){
+            n=(n&(n-1));
             res++;
         }
-        return res;
-    }
+        cout<<res;
+        return 0;
+}
+/*Logic:: Ex: 40
+		101000	(n = 40)
+	 	100111	(n-1 = 39)
+	______________________
+	&	100000	(n = 32)
+	
+		100000	(n = 32)
+		011111	(n = 31)
+	______________________
+	&	000000
+
+total iterations:	2
+total set bits: 	2
+Time Complexity = O(no. of set bits) 	
+
+*/
