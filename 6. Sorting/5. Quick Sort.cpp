52e8e73aa38548f9b05877e1c60e434d5e78a25f
M1yank <mbansal1_be19@thapar.edu>
2022-07-07 21:50:07 +0530

    Sorting

---
+++ 6. Sorting/5. Quick Sort.cpp
@@ -0,0 +1,59 @@
+// { Driver Code Starts
+#include <stdio.h>
+#include <bits/stdc++.h>
+using namespace std;
+
+/* Function to print an array */
+void printArray(int arr[], int size)
+{
+    int i;
+    for (i=0; i < size; i++)
+        printf("%d ", arr[i]);
+    printf("\n");
+}
+
+ // } Driver Code Ends
+class Solution
+{
+    public:
+    //Function to sort an array using quick sort algorithm.
+    void quickSort(int arr[], int low, int high)
+    {
+        if(low<high){
+            int pe=partition(arr,low,high);
+            quickSort(arr,low, pe-1);
+            quickSort(arr, pe+1, high);
+        }
+    }
+    
+    public:
+    int partition (int arr[], int low, int high)
+    {
+       int pivot = arr[high], i=low;
+       for(int j=low; j<high ; j++){
+           if(arr[j]<pivot){
+               swap(arr[i],arr[j]);
+               i++;
+           }
+       }
+       swap(arr[i], arr[high]);
+       return i;
+    }
+};
+
+
+// { Driver Code Starts.
+int main()
+{
+    int arr[1000],n,T,i;
+    scanf("%d",&T);
+    while(T--){
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+      scanf("%d",&arr[i]);
+      Solution ob;
+    ob.quickSort(arr, 0, n-1);
+    printArray(arr, n);
+    }
+    return 0;
+}  // } Driver Code Ends
