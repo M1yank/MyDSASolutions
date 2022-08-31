73a050dd2d025d36d8de5c63673063505b65aaaf
M1yank <mbansal1_be19@thapar.edu>
2022-09-01 01:49:02 +0530

    Graph

---
+++ 17. Graph/1. Print Adjacency List.cpp
@@ -0,0 +1,66 @@
+//{ Driver Code Starts
+//https://practice.geeksforgeeks.org/problems/print-adjacency-list-1587115620/1
+/*
+Input:
+1
+5 7
+0 1
+0 4
+1 2
+1 3
+1 4
+2 3
+3 4
+
+Output:
+0-> 1-> 4
+1-> 0-> 2-> 3-> 4
+2-> 1-> 3
+3-> 1-> 2-> 4
+4-> 0-> 1-> 3
+*/
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+  public:
+    // Function to return the adjacency list for each vertex.
+    vector<vector<int>> printGraph(int V, vector<int> adj[]) {
+        // Code here
+        vector<vector<int>> res(V);
+        for(int i=0; i<V; i++){
+            res[i].push_back(i);
+            for(auto x:adj[i]) res[i].push_back(x);
+        }
+        return res;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int tc;
+    cin >> tc;
+    while (tc--) {
+        int V, E;
+        cin >> V >> E;
+        vector<int> adj[V];
+        for (int i = 0; i < E; i++) {
+            int u, v;
+            cin >> u >> v;
+            adj[u].push_back(v);
+            adj[v].push_back(u);
+        }
+        Solution obj;
+        vector<vector<int>> ans = obj.printGraph(V, adj);
+        for (int i = 0; i < ans.size(); i++) {
+            for (int j = 0; j < ans[i].size() - 1; j++) {
+                cout << ans[i][j] << "-> ";
+            }
+            cout << ans[i][ans[i].size() - 1];
+            cout << endl;
+        }
+    }
+    return 0;
+}
+// } Driver Code Ends
